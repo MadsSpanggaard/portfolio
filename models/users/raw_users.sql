7797301e86f0dab3146392f67439850a14d4df7c
Mads <madsspanggaard1994@gmail.com>
2024-03-12 11:47:09 +0100

    quick fix

--- models/users/raw_users.sql
+++ models/users/raw_users.sql
@@ -1,5 +1,11 @@
 
 {{ config(materialized='table') }}  -- or 'table', if you prefer
 
-SELECT *
+SELECT 
+    "UserID" AS id,
+    "FirstName" AS  first_name,
+    "LAS tName" AS  last_name,
+    "Email" AS email,
+    "SignupDate" AS signup_date,
+    "Address" AS address 
 FROM {{ source('raw', 'users') }}
